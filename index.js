f4885927c77055a03f10ed031abe10aaf26c65af
AubreyNeimeier <neimeier.1@osu.edu>
2018-08-08 10:13:39 -0700

    completes trips and some relation unit tests

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let store = {drivers: [], passengers: [], trips: []}
 // initialize store with key of items and users that each point to an empty array
  
 let driverId = 0
+let passId = 0
+let tripId = 0
 
 class Driver{
     constructor(name){
@@ -19,7 +21,46 @@ class Driver{
 
     passengers(){
         return store.passengers.filter(pass =>{
-            pass.driverId === this.id
+            return pass.driverId === this.id
+        })
+    }
+}
+
+
+class Passenger{
+    constructor(name){
+        this.name = name;
+        this.id = ++ passId;
+    
+        store.passengers.push(this)
+    }
+    trips(){
+        return store.trips.filter(trip => {
+            return trip.passengerId = this.id
+        })
+    }
+
+
+}
+
+class Trip{
+    constructor(driver, passenger){
+        this.driverId = driver.id;
+        this.passengerId = passenger.id;
+        this.id = ++ tripId;
+
+        store.trips.push(this)
+    }
+
+    driver(){
+        return store.drivers.find(function(driver){
+            return driver.tripId === this.id
+        })
+    }
+
+    passenger(){
+        return store.passengers.find(function(pass){
+            return pass.tripId === this.id
         })
     }
 }
