8877f45f444760f864fc569852f919595e179739
AubreyNeimeier <neimeier.1@osu.edu>
2018-08-08 12:00:42 -0700

    adds lots of notes about arrow functions and this

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 let store = {drivers: [], passengers: [], trips: []}
 // initialize store with key of items and users that each point to an empty array
  
@@ -5,6 +7,9 @@ let driverId = 0
 let passId = 0
 let tripId = 0
 
+// a driver has many trips and HAS MANY passengers THROUGH trips. 
+// notice how the passengers methods reveals the passenger through the trips() method. 
+// trips is the 'join' table in this case. we use trips to find the passengers and drivers for each other because the driver does not have a passengerId attribute. 
 class Driver{
     constructor(name){
         this.name = name;
@@ -35,6 +40,8 @@ class Driver{
 
 */
 
+// passengers has many trips
+// and HAS MANY drivers THROUGH trips
 
 class Passenger{
     constructor(name){
@@ -50,8 +57,7 @@ class Passenger{
     }
 
     drivers(){
-        return this.trips().map(trip => {return trip.driver()
-        })
+        return this.trips().map(trip => {return trip.driver()})
     }
 
 
@@ -73,12 +79,30 @@ class Trip{
     }
 
     driver(){
-        return store.drivers.find(function(driver){
-            return driver.id === this.driverId}.bind(this))
+        return store.drivers.find(driver => {
+            return driver.id === this.driverId
+        });
     }
 
     passenger(){
-        return store.passengers.find(function(pass){
-            return pass.id === this.passengerId}.bind(this))
+        return store.passengers.find(passenger => {
+            return passenger.id === this.passengerId;
+          });
     }
+
+    //callback function with old school way (this is global), thus we use bind.
+    /* 
+    passenger() {
+        return store.passengers.find(function(passenger){
+          return passenger.id === this.passengerId}.bind(this))
+      }
+
+
+
+
+    using this arrow function :
+    An arrow function does not have its own this; the this value of the enclosing execution context is 
+    used. Thus, in the following code, the this within the function that is passed to setInterval has 
+    the same value as this in the enclosing function:
+    */
 }
